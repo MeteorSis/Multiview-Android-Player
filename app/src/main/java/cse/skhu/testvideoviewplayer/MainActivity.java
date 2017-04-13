2ec6a4e3e4776635b6a3bca5a936a77a2c693f42
MeteorSis <uncleone@naver.com>
2017-04-13 20:23:28 +0900

    test version upload

---
+++ app/src/main/java/cse/skhu/testvideoviewplayer/MainActivity.java
@@ -0,0 +1,30 @@
+package cse.skhu.testvideoviewplayer;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Locate the button in activity_main.xml
+        button = (Button) findViewById(R.id.MyButton);
+
+            // Capture button clicks
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View arg0) {
+            // Start NewActivity.class
+                Intent myIntent = new Intent(MainActivity.this, VideoViewActivity.class);
+                startActivity(myIntent);
+            }
+        });
+    }
+}
